8d52842d325cf67e341e3db8d3588464ff56c682
Caleb J Caldwell <ccaldw2@uwo.ca>
2018-05-13 16:03:43 -0400

    Started work on the De-Caps algorithm + updated version to 0.2

--- capsLockScript.js
+++ capsLockScript.js
@@ -1,1 +1,62 @@
-document.body.style.backgroundColor = "blue" ;
+document.title = generateReplacement(document.title);
+
+// Replace all initial text on page
+replaceNodeText(document.body);
+
+// Create a mutation observer to monitor the DOM for changes
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        Array.prototype.slice.call(mutation.addedNodes).forEach(replaceNodeText);
+    });
+});
+
+// Configure and start the observer
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: false,
+    characterData: false
+});
+
+
+function replaceNodeText(node) {
+
+    if(node.closest && node.closest('.kix-appview')) return;
+
+    var walker = document.createTreeWalker(
+        node,
+        NodeFilter.SHOW_TEXT,
+        {
+            acceptNode: function(node) {
+                // Reject contentEditable nodes
+                return (node.parentElement && node.parentElement.isContentEditable) ?
+                    NodeFilter.FILTER_SKIP :
+                    NodeFilter.FILTER_ACCEPT;
+            }
+        },
+        false
+    );
+
+    // Replace all text nodes
+    var textNode;
+    while(textNode = walker.nextNode()) {
+        textNode.nodeValue = generateReplacement(textNode.nodeValue);
+    }
+}
+/* TODO: FIX
+function generateReplacement(text) {
+    var regex = /[A-Z]/g;
+    var queue = [];
+    section = text;
+    while (text!=null){
+    	queue.push (regex.exec(section));
+    	section = section.slice(section.search(regex)+1,section.length);
+    }
+    if (section != null) { 
+    	var edit = section;
+    	//var end = section.charAt(length-1).toLowerCase();
+    	return text.replace(regex, queue.shift()).concat(end));
+	}
+}
+
+*/
