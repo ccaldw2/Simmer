b43a56c1fed2d45be0a449e9b96e4c17e01071b3
Caleb J Caldwell <ccaldw2@uwo.ca>
2018-05-13 10:56:08 -0400

    changed file names to be more specific, split app logic between 2 JS scripts

---
+++ appLogic.js
@@ -0,0 +1,4 @@
+/*
+	This file contains the application logic and algorithms used to parse text and convert
+	web pages as per the desired function of the software's design goals
+*/
---
+++ settings.js
@@ -0,0 +1,62 @@
+/*
+	This file contains all the scripts required to manage the memory of the simmer
+	extension, with regards to the application settings.
+*/
+
+
+/*
+	retrieves data stored via chrome.storage for each parameter of the application
+*/
+function retrieveSettings() {
+    chrome.storage.sync.get({"clap" : ""}, function(items) {
+    	console.log('retrieved clap as ' + items.clap);
+    	document.getElementById("clap").checked = items.clap;
+    });
+
+    chrome.storage.sync.get({"capsLock" : ""}, function(items) {
+    	console.log('retrieved capsLock as ' + items.capsLock);
+    	document.getElementById("capsLock").checked = items.capsLock;
+    });
+
+    chrome.storage.sync.get({"literally" : ""}, function(items) {
+    	console.log('retrieved literally as ' + items.literally);
+    	document.getElementById("literally").checked = items.literally;
+    });
+}
+
+/*
+	uses chrome.storage API to save settings for each parameter of the application
+	so that the application maintains continuity when the window is closed
+*/
+function changeSettings() {
+	var clap = document.getElementById("clap").checked;
+	var capsLock = document.getElementById("capsLock").checked;
+	var literally = document.getElementById("literally").checked;
+
+	chrome.storage.sync.set({"clap" : clap}, function() {
+		console.log('set clap to ' + clap);
+	});
+
+	chrome.storage.sync.set({"capsLock" : capsLock}, function() {
+		console.log('set capsLock to ' + capsLock);
+	});
+
+	chrome.storage.sync.set({"literally" : literally}, function() {
+		console.log('set literally to ' + literally);
+	});
+
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	retrieveSettings();
+
+	var listOfOptions = document.getElementsByClassName("selectionBox")
+
+		for(var i = 0; i < listOfOptions.length; i++) { 
+			listOfOptions[i].addEventListener("click", function() {
+				changeSettings();
+			})
+		}
+})
