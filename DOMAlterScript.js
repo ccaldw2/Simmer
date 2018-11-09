e78de1a08570daa7f088e5d437fe6f0ac67cd541
Caleb J Caldwell <ccaldw2@uwo.ca>
2018-11-09 14:24:28 -0500

    consolidated multiple scripts to a single extendable file, refactored according to Robert Martin's Clean Code principles

---
+++ DOMAlterScript.js
@@ -0,0 +1,74 @@
+(function () {
+    chrome.storage.onChanged.addListener( function() {
+            
+        chrome.storage.sync.get(["literally","capsLock"], function(items) {
+            replaceTitleText(items)
+            replaceNodeText(document.body, items);
+        })
+
+    })
+})()
+
+
+
+function replaceTitleText(conditions) {
+    document.title = delegateEdits(document.title, conditions)
+}
+
+function replaceNodeText(node, conditions) {
+
+    if (node.closest && node.closest('.kix-appview')) return;
+
+    var walker = document.createTreeWalker(
+        node,
+        NodeFilter.SHOW_TEXT,
+        {
+            acceptNode: function(node) {
+                return (node.parentElement && node.parentElement.isContentEditable) ?
+                    NodeFilter.FILTER_SKIP :
+                    NodeFilter.FILTER_ACCEPT;
+            }
+        },
+        false
+    );
+
+    var textNode;
+    while(textNode = walker.nextNode()) {
+        textNode.nodeValue = delegateEdits(textNode.nodeValue,conditions);
+    }
+}
+
+
+
+function delegateEdits(text, conditions) {
+    if (conditions["literally"] === true)
+        text = removeLiterallys(text)
+    if (conditions["capsLock"] === true) 
+        text = deCapitalise(text)
+    return text;
+}
+
+
+
+function removeLiterallys(text) {
+    var regex = /\bliterally\b/gi;
+
+    return text.replace(regex, " ");
+}
+
+function deCapitalise(text) {
+    var regex = RegExp(/[A-Z]/g);
+
+    while(text.search(regex) !== -1) {
+        var loc = text.search(regex);
+        var replacement = text.charAt(loc).toLowerCase();
+        text = text.replaceAt(loc,replacement);
+    }
+    return text;
+}
+
+
+
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
--- appLogic.js
+++ appLogic.js
@@ -1,22 +1,8 @@
-/*
-	This file contains the application logic and algorithms used to parse text and convert
-	web pages as per the desired function of the software's design goals
-*/
-
-
-
 document.addEventListener("DOMContentLoaded", function() {
-
-	var listOfOptions = document.getElementsByClassName("selectionBox")
-
-	for(var i = 0; i < listOfOptions.length; i++) { 
-		listOfOptions[i].addEventListener("click", function() {
-			sweepWebsite();
-		});
-	}
+  sweepWebsite();
 })
 
-function sweepWebsite(){
+function sweepWebsite() {
   	var queryInfo = {
     	active: true,
     	currentWindow: true
@@ -24,7 +10,7 @@ function sweepWebsite(){
 
   	var tab; 
   	chrome.tabs.query (queryInfo, function(tabs){
-		tab = tabs[0];
+		  tab = tabs[0];
   	});
 
   	injectScript ();
@@ -32,23 +18,8 @@ function sweepWebsite(){
 }
 
 function injectScript() {
-
-	var scripts = ['capsLockScript.js', 'literallyScript.js'];
-
-    chrome.storage.sync.get({"capsLock" : ""}, function(items) {
-    	if(items.capsLock){
-	    	chrome.tabs.executeScript({
-	    		file: scripts[0]
-  			});
-    	};
-    });
-
-    chrome.storage.sync.get({"literally" : ""}, function(items) {
-    	if(items.literally){
-	    	chrome.tabs.executeScript({
-	    		file: scripts[1]
-  			});
-    	};
-    });
+	chrome.tabs.executeScript(
+    {file: "DOMAlterScript.js"}
+  );
 }
 
